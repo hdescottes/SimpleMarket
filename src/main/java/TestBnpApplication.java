9232027db6150fad078958c5eab9ca0c41a1c91b
HugoD <hudescottes@hotmail.fr>
2019-09-06 11:26:02 +0200

    first commit

---
+++ src/main/java/TestBnpApplication.java
@@ -0,0 +1,13 @@
+package main.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBnpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBnpApplication.class, args);
+    }
+
+}
