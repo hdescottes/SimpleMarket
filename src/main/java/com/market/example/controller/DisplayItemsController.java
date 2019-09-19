d8f15f025696f19493f14c3cbee3280fe4544d7a
HugoD <hudescottes@hotmail.fr>
2019-09-19 11:02:16 +0200

    Add Controller tests

--- src/main/java/com/market/example/controller/DisplayItemsController.java
+++ src/main/java/com/market/example/controller/DisplayItemsController.java
@@ -1,18 +1,14 @@
 package com.market.example.controller;
 
-import com.market.example.model.FruitDto;
 import com.market.example.service.MarketService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
--- src/test/java/com/market/example/controller/DisplayItemsControllerTests.java
+++ src/test/java/com/market/example/controller/DisplayItemsControllerTests.java
@@ -1,13 +1,42 @@
 package com.market.example.controller;
 
 import com.market.example.ApplicationTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ApplicationTest
 class DisplayItemsControllerTests {
 
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
     @Test
-    void retrieveMarketItems_ShouldSucceed() {
-        
+    void retrieveMarketItems_ShouldSucceed() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("items"))
+                .andExpect(view().name("marketList"));
+    }
+
+    @Test
+    void retrieveBasketPrice_ShouldSucceed() throws Exception {
+        this.mockMvc.perform(get("/basketList"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("map"))
+                .andExpect(view().name("basketList"));
     }
 }
